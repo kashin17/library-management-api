f1184ae2fdfb1dc508fb811aeaa2b8766846e278
kashin17 <karora504@gmail.com>
2025-05-29 18:51:49 +0530

    vercel 21

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
       "frame-src 'self';"
     );
   }
-  // next();
+  next();
 });
 
 
@@ -213,10 +213,5 @@ app.use((err, req, res, next) => {
 //   res.status(500).json({ message: 'Something went wrong!' });
 // });
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-//   console.log(`API Documentation available at http://localhost:${PORT}/api-docs`);
-// });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = require('./app');
+// const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
